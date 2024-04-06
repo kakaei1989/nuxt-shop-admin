4cee9643e4521b2d28f3e1d3dd6321db72fde94d
Ahmad Kakaei <kakaei1989@gmail.com>
2024-04-06 13:09:01 +0330

    add admin panel layout

---
+++ layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+    <LayoutHeader />
+
+    <div class="container-fluid">
+        <div class="row">
+            <LayoutSidebar />
+            <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                <slot />
+            </main>
+        </div>
+    </div>
+</template>
