8c3bd1e56bddcb40b250345827563dd4cf6e7217
Ahmad Kakaei <kakaei1989@gmail.com>
2024-04-10 16:59:30 +0330

    add get users api

---
+++ pages/users/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div
+        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h4 class="fw-bold">کاربران</h4>
+        <button type="button" class="btn btn-sm btn-outline-dark">
+            ایجاد کاربر
+        </button>
+    </div>
+
+    <UserList :users="data.users" />
+
+    <div class="d-flex justify-content-center">
+        <nav aria-label="navigation">
+            <ul class="pagination">
+                <li v-for="(link, index) in data.meta.links.slice(1, -1)" :key="index" class="page-item"
+                    :class="{ active: link.active }">
+                    <button @click="paginate(link.label)" class="page-link">{{
+                        link.label
+                    }}</button>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+const page = ref(0);
+
+const { data, refresh } = await useFetch(() => '/api/global', {
+    query: { url: '/users', page },
+    headers: useRequestHeaders(['cookie'])
+})
+
+function paginate(value) {
+    page.value = value;
+    refresh()
+}
+
+</script>
--- server/api/global.get.js
+++ server/api/global.get.js
@@ -3,8 +3,10 @@ export default defineEventHandler(async (event) => {
     const token = getCookie(event, 'token')
     const query = getQuery(event);
 
+    let page = query.hasOwnProperty('page') ? `?page=${query.page}` : '';
+
     try {
-        const data = await $fetch(`${apiBase}${query.url}`, {
+        const data = await $fetch(`${apiBase}${query.url}${page}`, {
             headers: {
                 'Accept': 'application/json',
                 'Authorization': `Bearer ${token}`
