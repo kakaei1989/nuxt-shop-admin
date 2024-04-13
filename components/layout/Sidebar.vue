c73fa3b8db8b7b6edd66e28595744faf243fcbbc
Ahmad Kakaei <kakaei1989@gmail.com>
2024-04-13 22:24:15 +0330

    add transactions page

--- components/layout/Sidebar.vue
+++ components/layout/Sidebar.vue
@@ -33,10 +33,10 @@
                   </NuxtLink>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">
-                        <i class="bi bi-currency-dollar me-2"></i>
-                        تراکنش ها
-                    </a>
+                  <NuxtLink class="nav-link" :class="{ active: $route.path.includes('/transactions') }" to="/transactions">
+                    <i class="bi bi-grid-3x3-gap me-2"></i>
+                    تراکنش ها
+                  </NuxtLink>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="#">
