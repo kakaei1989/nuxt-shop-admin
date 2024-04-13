35b5de982c95b30936a84a2946bb85d088554eb8
Ahmad Kakaei <kakaei1989@gmail.com>
2024-04-13 20:39:55 +0330

    get and show categories

---
+++ components/category/Delete.vue
@@ -0,0 +1,34 @@
+<template>
+    <button @click="deleteCategory" :disabled="loading" class="btn btn-dark mt-5">
+        حذف
+        <div v-if="loading" class="spinner-border spinner-border-sm ms-2"></div>
+    </button>
+</template>
+
+<script setup>
+import { useToast } from "vue-toastification";
+
+const props = defineProps(['categoryId']);
+
+const loading = ref(false);
+const toast = useToast();
+
+async function deleteCategory() {
+    try {
+        loading.value = true;
+
+        await $fetch('/api/global', {
+            method: 'DELETE',
+            query: { url: `/categories/${props.userId}` },
+        })
+
+        toast.warning("حذف دسته بندی باموفقیت انجام شد");
+        return navigateTo('/categories')
+    } catch (error) {
+        console.log(error.data);
+    } finally {
+        loading.value = false;
+    }
+}
+
+</script>
---
+++ pages/categories/index.vue
@@ -0,0 +1,43 @@
+<template>
+    <div
+        class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h4 class="fw-bold">دسته بندی</h4>
+        <NuxtLink to="/categories/create" class="btn btn-sm btn-outline-dark">
+            ایجاد دسته بندی
+        </NuxtLink>
+    </div>
+
+    <CategoryList :categories="data.categories" />
+
+    <div class="d-flex justify-content-center">
+        <nav aria-label="navigation">
+            <ul class="pagination">
+                <li v-for="(link, index) in data.meta.links.slice(1, -1)" :key="index" class="page-item"
+                    :class="{ active: link.active }">
+                    <button @click="paginate(link.label)" class="page-link">{{
+                        link.label
+                    }}</button>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    middleware: 'auth'
+})
+
+const page = ref(0);
+
+const { data, refresh } = await useFetch(() => '/api/global', {
+    query: { url: '/categories', page },
+    headers: useRequestHeaders(['cookie'])
+})
+
+function paginate(value) {
+    page.value = value;
+    refresh()
+}
+
+</script>
